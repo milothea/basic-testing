9cdbd2098d1ca826dbd2d4c55ce131d234a8e5a0
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-07-01 10:22:10 +0200

    feat: partly cover '04-test-class' with tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,65 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  getBankAccount,
+  InsufficientFundsError,
+  TransferFailedError,
+} from './index';
 
 describe('BankAccount', () => {
+  let account: BankAccount;
+  let beneficiary: BankAccount;
+
+  beforeEach(() => (account = getBankAccount(100500)));
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(getBankAccount(0)).toBeInstanceOf(BankAccount);
+    expect(getBankAccount(50).getBalance()).toBe(50);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => account.withdraw(100501)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    beneficiary = getBankAccount(0);
+
+    expect(() => account.transfer(100501, beneficiary)).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => account.transfer(100500, account)).toThrow(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    expect(account.getBalance()).toBe(100500);
+
+    account.deposit(1);
+
+    expect(account.getBalance()).toBe(100501);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    expect(account.getBalance()).toBe(100500);
+
+    account.withdraw(500);
+
+    expect(account.getBalance()).toBe(100000);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    beneficiary = getBankAccount(0);
+
+    expect(account.getBalance()).toBe(100500);
+    expect(beneficiary.getBalance()).toBe(0);
+
+    account.transfer(500, beneficiary);
+
+    expect(account.getBalance()).toBe(100000);
+    expect(beneficiary.getBalance()).toBe(500);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
